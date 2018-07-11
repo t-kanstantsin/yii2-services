c8f37b6cf01985dfe664fa9639e2eb8025f04952
konstantin <k.timoshenko@kakadu.bz>
2018-07-11 11:44:20 +0300

    service config - add Yii2 behavior-like configuration for services
    - services method not static anymore

--- src/ImplementServices.php
+++ src/ImplementServices.php
@@ -9,6 +9,7 @@
 namespace MP\Services;
 
 use yii\base\BaseObject;
+use yii\base\InvalidConfigException;
 use yii\base\UnknownMethodException;
 use yii\base\UnknownPropertyException;
 
@@ -23,38 +24,60 @@ use yii\base\UnknownPropertyException;
 trait ImplementServices
 {
     /**
-     * @var array
+     * @var IService[]
      */
     private $servicesInstances = [];
 
     /**
-     * Array classname services
+     * Returns a list of services that this trait should attach to class.
      *
-     * @return array
+     * The return value of this method should be an array of configurations
+     * indexed by services names. A service configuration can be either a string specifying
+     * the service class or an array of the following structure:
+     *
+     * ```php
+     * 'serviceName1' => 'ServiceClass',
+     * 'serviceName2' => [
+     *     'class' => 'ServiceClass',
+     *     'property1' => 'value1',
+     *     'property2' => 'value2',
+     * ],
+     * ```
+     *
+     * Note that a service class must implement from [[IService]].
+     *
+     * Services declared in this method will be generated to the object automatically (on demand).
+     *
+     * @return array the service configurations.
      */
-    public static function services(): array
+    public function services(): array
     {
         return [];
     }
 
     /**
      * @inheritdoc
+     * @throws UnknownPropertyException
+     * @throws InvalidConfigException
      */
     public function __get($name)
     {
         try {
             return parent::__get($name);
         } catch (UnknownPropertyException $exception) {
-            if ($service = $this->getService($name)) {
-                return $service;
+            $service = $this->getService($name);
+
+            if ($service === NULL) {
+                throw new UnknownPropertyException('', 0, $exception);
             }
 
-            throw new UnknownPropertyException('', 0, $exception);
+            return $service;
         }
     }
 
     /**
      * @inheritdoc
+     * @throws InvalidConfigException
      */
     public function __call($name, $params)
     {
@@ -62,12 +85,13 @@ trait ImplementServices
             return parent::__call($name, $params);
         } catch (UnknownMethodException $exception) {
             $sName = lcfirst(substr($name, 3, strlen($name)));
+            $service = $this->getService($sName, (array) $params);
 
-            if ($service = $this->getService($sName, (array) $params)) {
-                return $service;
+            if ($service === NULL) {
+                throw new UnknownMethodException('', 0, $exception);
             }
 
-            throw new UnknownMethodException('', 0, $exception);
+            return $service;
         }
     }
 
@@ -78,20 +102,48 @@ trait ImplementServices
      * @param array  $params
      *
      * @return IService|null
+     * @throws InvalidConfigException
      */
     private function getService(string $name, array $params = []): ?IService
     {
-        $className = $this->getClassName($name);
+        if (array_key_exists($name, $this->servicesInstances)) {
+            return $this->servicesInstances[$name];
+        }
 
-        if ($className === NULL) {
+        $serviceConfig = $this->getServiceConfig($name);
+        if ($serviceConfig === NULL) {
             return NULL;
         }
 
-        if (!isset($this->servicesInstances[$name])) {
-            $this->servicesInstances[$name] = new $className($this, isset($params[0]) && is_array($params[0]) ? $params[0] : []);
+        $class  = NULL;
+        $config = [];
+        if (\is_string($serviceConfig)) {
+            $class  = $serviceConfig;
+            $config = [];
+        } elseif (\is_array($serviceConfig)) {
+            $class  = $serviceConfig['class'] ?? NULL;
+            unset($serviceConfig['class']);
+            $config = $serviceConfig;
+        }
+        $config = array_merge($config, isset($params[0]) && \is_array($params[0]) ? $params[0] : []);
+
+        if ($class === NULL) {
+            throw new InvalidConfigException('Object configuration must contain a "class" element.');
         }
 
-        return $this->servicesInstances[$name];
+        if (\is_subclass_of($class, BaseModelService::class)
+            || \is_subclass_of($class, BaseControllerService::class)
+        ) {
+            $service = new $class($this, $config);
+        } else {
+            $service = new $class($config);
+        }
+
+        if (!($service instanceof IService)) {
+            throw new InvalidConfigException('Service class must implement `' . IService::class . '`.');
+        }
+
+        return $this->servicesInstances[$name] = $service;
     }
 
     /**
@@ -99,10 +151,10 @@ trait ImplementServices
      *
      * @param string $name
      *
-     * @return string|null
+     * @return array|string|NULL
      */
-    private function getClassName(string $name): ?string
+    private function getServiceConfig(string $name)
     {
-        return static::services()[$name] ?? NULL;
+        return $this->services()[$name] ?? NULL;
     }
 }
